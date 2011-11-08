de50ea63f4ceecc0f7219001aefebf97831daefd
Bill Kirtley <bill@actbluetech.com>
2011-11-08 12:49:48 -0500

    one comma in the wrong place can ruin your day

--- lib/salsa_supporter.rb
+++ lib/salsa_supporter.rb
@@ -1,5 +1,5 @@
 class SalsaSupporter < SalsaObject
-  salsa_attribute :organization_KEY, :Email, :First_Name, :Last_Name, :Phone,
+  salsa_attribute :organization_KEY, :Email, :First_Name, :Last_Name, :Phone
   salsa_attribute :Street, :Street_2
   salsa_attribute :City, :State, :Country, :Zip
   salsa_attribute :Organization, :Occupation
